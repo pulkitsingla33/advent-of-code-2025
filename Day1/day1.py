c1d964e6cc4ca82ec93c2e161613c6596ded13fd
PulkitSingla2003 <f20212588@goa.bits-pilani.ac.in>
2025-12-01 13:45:05 +0530

    Completed Day 1 Puzzle Code

--- Day1/day1.py
+++ Day1/day1.py
@@ -3,30 +3,50 @@ zero_counter = 0
 
 
 def find_new_position(current_position, direction, distance):
+    
+    """
+    Find a new position given the current position, direction, and distance.
+
+    Args:
+    current_position (int): The current position.
+    direction (str): The direction to move ('L' or 'R').
+    distance (int): The distance to move.
+
+    Returns:
+    tuple: A tuple containing the new position and the number of zeros crossed.
+    """
+    zero_crossing = abs(distance // 100)
     distance = distance % 100
     if(direction == 'L'):
         new_pos = current_position - distance
         if(new_pos < 0):
             new_pos = 100 + new_pos
+            if(current_position != 0):
+                zero_crossing += 1
     else:
         new_pos = current_position + distance
         if(new_pos >= 100):
             new_pos = new_pos - 100
-    return new_pos
+            if(new_pos != 0):
+                zero_crossing += 1
+    return (new_pos, zero_crossing)
+
+
 
 with open('input.txt') as f:
     lines = [line.strip() for line in f]
-    # print(lines)
-
 
 
 for line in lines:
     direction, distance = line[0], line[1:]
-    print("Current Position:", current_position)
-    current_position = find_new_position(current_position, direction, int(distance))
+   
+    distance = int(distance)
+
+    current_position, zero_crossing = find_new_position(current_position, direction, distance)
     
     if(current_position == 0):
         zero_counter += 1
+    zero_counter += zero_crossing
 
 print("Zeros encountered:", zero_counter)
 print("Total Lines Processed:", len(lines))
